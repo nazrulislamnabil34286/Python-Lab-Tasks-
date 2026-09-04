7fd0b86e0f561dfb6e6711f4155aa198ee120926
Nazrul islam nabil <nazrulislam34286@gmail.com>
2026-09-04 20:55:41 +0600

    Added Lab10

---
+++ lab10 (NumPy)/lab10(problem4).py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = np.array([10, 20, 30])
+y = np.array([1, 2, 3])
+
+print("First Array:")
+print(x)
+
+print("\nSecond Array:")
+print(y)
+
+print("\nAddition:")
+print(np.add(x, y))
+
+print("\nSubtraction:")
+print(np.subtract(x, y))
+
+print("\nMultiplication:")
+print(np.multiply(x, y))
+
+print("\nDivision:")
+print(np.divide(x, y))
